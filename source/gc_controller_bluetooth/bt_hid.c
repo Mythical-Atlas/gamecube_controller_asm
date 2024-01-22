f63ea5c7b991ee7ffe7bd4360e66e6f1d2f75ebe
Ben Correll <ben@robertcorrell.com>
2024-01-21 20:49:39 -0500

    Cleaning up bluetooth code.

--- source/gc_controller_bluetooth/bt_hid.c
+++ source/gc_controller_bluetooth/bt_hid.c
@@ -44,7 +44,7 @@
 #include <string.h>
 
 #include "pico/stdlib.h"
-//#include "pico/cyw43_arch.h"
+#include "pico/cyw43_arch.h"
 #include "pico/async_context.h"
 
 #include "btstack_run_loop.h"
@@ -246,35 +246,24 @@ static void try_connect()
     {
         printf("hid_host_connect command failed: 0x%02x\n", status);
         bt_hid_disconnected(connected_addr);
-        //try_connect();
     }
 }
 
-static void packet_handler (uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size)
+static void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size)
 {
-	UNUSED(channel);
-	UNUSED(size);
+	if(packet_type != HCI_EVENT_PACKET) return;
 
-	uint8_t   event;
-	uint8_t   hid_event;
-	bd_addr_t event_addr;
-	uint8_t   status;
-	uint8_t reason;
-
-	if (packet_type != HCI_EVENT_PACKET) {
-		return;
-	}
-
-	event = hci_event_packet_get_type(packet);
-	switch (event) {
+	uint8_t event = hci_event_packet_get_type(packet);
+	switch(event)
+    {
 	case BTSTACK_EVENT_STATE:
-		// On boot, we try a manual connection
-		if (btstack_event_state_get_state(packet) == HCI_STATE_WORKING){
+		if (btstack_event_state_get_state(packet) == HCI_STATE_WORKING)
+        {
 			try_connect();
 		}
 		break;
 	case HCI_EVENT_CONNECTION_COMPLETE:
-		status = hci_event_connection_complete_get_status(packet);
+		uint8_t status = hci_event_connection_complete_get_status(packet);
 		printf("Connection complete: %x\n", status);
 
         if(status == 4)
@@ -285,98 +274,64 @@ static void packet_handler (uint8_t packet_type, uint16_t channel, uint8_t *pack
 		break;
 	case HCI_EVENT_DISCONNECTION_COMPLETE:
 		status = hci_event_disconnection_complete_get_status(packet);
-		reason = hci_event_disconnection_complete_get_reason(packet);
+		uint8_t reason = hci_event_disconnection_complete_get_reason(packet);
 		printf("Disconnection complete: status: %x, reason: %x\n", status, reason);
 
         bt_hid_disconnected(connected_addr);
         try_connect();
 
 		break;
-	case HCI_EVENT_MAX_SLOTS_CHANGED:
-		status = hci_event_max_slots_changed_get_lmp_max_slots(packet);
-		//printf("Max slots changed: %x\n", status);
-		break;
-	case HCI_EVENT_USER_CONFIRMATION_REQUEST:
-		//printf("SSP User Confirmation Request: %d\n", little_endian_read_32(packet, 8));
-		break;
 	case HCI_EVENT_HID_META:
-		hid_event = hci_event_hid_meta_get_subevent_code(packet);
-		switch (hid_event) {
+		uint8_t hid_event = hci_event_hid_meta_get_subevent_code(packet);
+		switch(hid_event)
+        {
 		case HID_SUBEVENT_CONNECTION_OPENED:
 			status = hid_subevent_connection_opened_get_status(packet);
+
+            bd_addr_t event_addr;
 			hid_subevent_connection_opened_get_bd_addr(packet, event_addr);
-			if (status != ERROR_CODE_SUCCESS) {
+
+			if(status != ERROR_CODE_SUCCESS)
+            {
 				printf("Connection to %s failed: 0x%02x\n", bd_addr_to_str(event_addr), status);
 				bt_hid_disconnected(event_addr);
 				return;
 			}
+
 			hid_host_descriptor_available = false;
 			hid_host_cid = hid_subevent_connection_opened_get_hid_cid(packet);
+
 			printf("Connected to %s\n", bd_addr_to_str(event_addr));
+
 			bd_addr_copy(connected_addr, event_addr);
 			break;
 		case HID_SUBEVENT_DESCRIPTOR_AVAILABLE:
 			status = hid_subevent_descriptor_available_get_status(packet);
-			if (status == ERROR_CODE_SUCCESS){
+			if(status == ERROR_CODE_SUCCESS)
+            {
 				hid_host_descriptor_available = true;
-
-                /*
-				uint16_t dlen = hid_descriptor_storage_get_descriptor_len(hid_host_cid);
-				printf("HID descriptor available. Len: %d\n", dlen);
-
-                printf_hexdump(hid_descriptor_storage_get_descriptor_data(hid_host_cid), dlen);
-                */
-
-				// Send FEATURE 0x05, to switch the controller to "full" report mode
-				//hid_host_send_get_report(hid_host_cid, HID_REPORT_TYPE_FEATURE, 0x05);
-			} else {
-				printf("Couldn't process HID Descriptor, status: %d\n", status);
 			}
 			break;
 		case HID_SUBEVENT_REPORT:
-            if (hid_host_descriptor_available){
-                //if(hci_event_hid_meta_get_subevent_code(packet) == 0x3f)
-                //{
-                    //printf("len: %i\npacket:\n", size);
-                    //printf_hexdump(packet, size);
-                //}
-
+            if(hid_host_descriptor_available)
+            {
                 hid_host_handle_interrupt_report(packet, size);
-
-				//hid_host_handle_interrupt_report(hid_subevent_report_get_report(packet), hid_subevent_report_get_report_len(packet));
-			} else {
-				printf("No hid host descriptor available\n");
-				printf_hexdump(hid_subevent_report_get_report(packet), hid_subevent_report_get_report_len(packet));
 			}
 			break;
 		case HID_SUBEVENT_CONNECTION_CLOSED:
 			printf("HID connection closed: %s\n", bd_addr_to_str(connected_addr));
 			bt_hid_disconnected(connected_addr);
 			break;
-		default:
-			printf("Unknown HID subevent: 0x%x\n", hid_event);
-			break;
 		}
-		break;
-	default:
-		//printf("Unknown HCI event: 0x%x\n", event);
 		break;
 	}
 }
 
 void bt_main(void) {
-	if (cyw43_arch_init()) {
-		//printf("Wi-Fi init failed\n");
-		return;
-	}
-
+	cyw43_arch_init();
 	gap_set_security_level(LEVEL_2);
-
 	hid_host_setup();
 	sscanf_bd_addr(remote_addr_string, remote_addr);
-	bt_hid_disconnected(remote_addr);
-
 	hci_power_control(HCI_POWER_ON);
-
 	btstack_run_loop_execute();
 }
